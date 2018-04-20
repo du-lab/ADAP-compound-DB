7a936ebe8cfc84cb16774dfea5d7d34311d66d89
Aleksandr Smirnov <asmirno1@uncc.edu>
2018-04-20 09:16:19 -0400

    Write a simple test with Mockito

--- src/main/test/org/dulab/site/services/AuthenticationServiceImplTest.java
+++ src/main/test/org/dulab/site/services/AuthenticationServiceImplTest.java
@@ -2,8 +2,12 @@ package org.dulab.site.services;
 
 import org.dulab.config.ApplicationContextConfiguration;
 import org.dulab.config.ServletContextConfiguration;
+import org.dulab.models.UserPrincipal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mindrot.jbcrypt.BCrypt;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,22 +18,23 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {
-        ApplicationContextConfiguration.class,
-        ServletContextConfiguration.class
-})
-@ActiveProfiles(profiles = "test")
-@WebAppConfiguration
-@Transactional
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {
+//        ApplicationContextConfiguration.class,
+//        ServletContextConfiguration.class
+//})
+//@ActiveProfiles(profiles = "test")
+//@WebAppConfiguration
+//@Transactional
+@RunWith(MockitoJUnitRunner.class)
 public class AuthenticationServiceImplTest {
 
-    @Autowired
-    AuthenticationService authenticationService;
+    @Mock
+    private AuthenticationService authenticationService;
 
     @Test
     public void saveUser() {
 
-        assertTrue(authenticationService instanceof AuthenticationServiceImpl);
+        assertNotNull(authenticationService);
     }
 }
