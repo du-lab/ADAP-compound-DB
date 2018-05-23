0b8d93157e60016ab2e7ae2ecf8f585f0e7215fc
architkhullar <architkhullar@yahoo.com>
2018-05-23 14:01:59 -0400

    adding new tester

---
+++ src/main/test/org/dulab/adapcompounddb/site/controllers/FileUploadControllerTest.java
@@ -0,0 +1,27 @@
+package site.controllers;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileUploadControllerTest extends TestCase {
+
+    @Test
+    public void addAttributes() {
+
+        //testing this
+
+
+    }
+
+    @Test
+    public void upload() {
+
+    }
+
+    @Test
+    public void upload1() {
+
+    }
+}
