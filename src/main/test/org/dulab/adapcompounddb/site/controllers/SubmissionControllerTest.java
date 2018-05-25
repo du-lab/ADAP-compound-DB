f7f3b9759650c20b9d592c75eb35d3425ece5d0c
architkhullar <architkhullar@yahoo.com>
2018-05-25 16:58:05 -0400

    create JUnit test for SubmissionController

--- src/main/test/org/dulab/adapcompounddb/site/controllers/SubmissionControllerTest.java
+++ src/main/test/org/dulab/adapcompounddb/site/controllers/SubmissionControllerTest.java
@@ -1,10 +1,64 @@
 package site.controllers;
 
 import junit.framework.TestCase;
+import org.dulab.adapcompounddb.config.ServletContextConfiguration;
+import org.dulab.adapcompounddb.site.controllers.AuthenticationController;
+import org.dulab.adapcompounddb.site.controllers.IndexController;
+import org.dulab.adapcompounddb.site.controllers.SubmissionController;
+import org.dulab.adapcompounddb.site.services.SpectrumService;
+import org.dulab.adapcompounddb.site.services.SubmissionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SubmissionControllerTest extends TestCase {
 
+    private MockMvc mockMvc;
+    private MockHttpSession mockHttpSession;
+
+    @Mock
+    private SubmissionService submissionService;
+    private SpectrumService spectrumService;
+
+
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new SubmissionController(submissionService, spectrumService), new IndexController())
+                .setViewResolvers(
+                        new ServletContextConfiguration(
+                                new LocalValidatorFactoryBean()).viewResolver())
+                .build();
+
+        mockHttpSession = new MockHttpSession();
+
+    }
+
+    @Test
+    public void fileViewTest() throws Exception {
+
+        mockMvc.perform(get("/file/"))
+                .andExpect(status().is3xxRedirection())// checks the status
+                .andExpect(view().name("redirect:/file/upload/"));// checks the view name
+                //.andExpect(forwardedUrl("/WEB-INF/jsp/view/login.jsp"));  // checks the view filename
+
+    }
+
+    @Test
+    public void viewSubmissionTest() throws Exception{
+
+
     }
 }
