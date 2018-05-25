11d9c2e8760c4b576ddf869d2eb509f89d0329ca
architkhullar <architkhullar@yahoo.com>
2018-05-25 14:18:20 -0400

    create JUnit test for SubmissionController

---
+++ src/main/test/org/dulab/adapcompounddb/site/controllers/SubmissionControllerTest.java
@@ -0,0 +1,10 @@
+package site.controllers;
+
+import junit.framework.TestCase;
+
+public class SubmissionControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+}
