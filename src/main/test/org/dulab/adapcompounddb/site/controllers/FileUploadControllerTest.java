d9a9fc296d492907b3108c7796111f72ec464248
architkhullar <architkhullar@yahoo.com>
2018-05-29 15:24:20 -0400

    JUnit test for SubmissionController finished

--- src/main/test/org/dulab/adapcompounddb/site/controllers/FileUploadControllerTest.java
+++ src/main/test/org/dulab/adapcompounddb/site/controllers/FileUploadControllerTest.java
@@ -1,16 +1,11 @@
-package site.controllers;
+package org.dulab.adapcompounddb.site.controllers;
 
 import junit.framework.TestCase;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.dulab.adapcompounddb.config.ServletContextConfiguration;
-import org.dulab.adapcompounddb.models.FileType;
 import org.dulab.adapcompounddb.models.entities.Submission;
-import org.dulab.adapcompounddb.models.entities.UserPrincipal;
-import org.dulab.adapcompounddb.site.controllers.AuthenticationController;
-import org.dulab.adapcompounddb.site.controllers.FileUploadController;
-import org.dulab.adapcompounddb.site.controllers.IndexController;
-import org.dulab.adapcompounddb.site.services.FileReaderService;
+import org.dulab.adapcompounddb.site.services.SpectrumService;
+import org.dulab.adapcompounddb.site.services.SubmissionService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,11 +15,6 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -37,10 +27,17 @@ public class FileUploadControllerTest extends TestCase {
     @Mock
     private Submission submission;
 
+    @Mock
+    private SubmissionService submissionService;
+
+    @Mock
+    private SpectrumService spectrumService;
+
     @Before
     public void SetUp() {
         mockMvc = MockMvcBuilders
-                .standaloneSetup(new FileUploadController())
+                .standaloneSetup(new FileUploadController(),
+                        new SubmissionController(submissionService,spectrumService))
                 .setViewResolvers(
                         new ServletContextConfiguration(
                                 new LocalValidatorFactoryBean()).viewResolver())
@@ -62,13 +59,14 @@ public class FileUploadControllerTest extends TestCase {
     @Test
     public void FileUploadRedirectTest() throws Exception{
         Submission.assign(mockHttpSession, submission);
+
         mockMvc.perform(get("/file/upload/").session(mockHttpSession))
                 .andExpect(status().is3xxRedirection()) // checks the status
-                .andExpect(redirectedUrl("/file/?chromatographyTypeList=GAS&chromatographyTypeList=LIQUID_POSITIVE&chromatographyTypeList=LIQUID_NEGATIVE&fileTypeList=MSP"));  // check the redirect url
+                .andExpect(redirectedUrlPattern("/file/*"));  // check the redirect url
 
         //Check that the redirected url is handled
-        //mockMvc.perform(get("/file/?chromatographyTypeList=GAS&chromatographyTypeList=LIQUID_POSITIVE&chromatographyTypeList=LIQUID_NEGATIVE&fileTypeList=MSP"))
-          //      .andExpect(status().isOk());  // checks the status
+        mockMvc.perform(get("/file/").session(mockHttpSession))
+                .andExpect(status().isOk());  // checks the status
 
     }
 }
