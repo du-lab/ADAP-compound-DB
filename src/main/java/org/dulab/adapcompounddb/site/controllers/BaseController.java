16f756a5d1827df2288d5652d5908d8b1f3c4493
Nilanjan <nilanjan.mhatre@gmail.com>
2018-07-19 16:23:25 -0400

    add a basecontroller

---
+++ src/main/java/org/dulab/adapcompounddb/site/controllers/BaseController.java
@@ -0,0 +1,42 @@
+package org.dulab.adapcompounddb.site.controllers;
+
+import org.dulab.adapcompounddb.models.entities.UserPrincipal;
+import org.dulab.adapcompounddb.site.services.UserPrincipalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+public abstract class BaseController {
+
+	@Autowired
+	UserPrincipalService userPrincipalService;
+
+	public SecurityContext getSecurityContext() {
+		return SecurityContextHolder.getContext();
+	}
+
+    public UserPrincipal getCurrentUserPrincipal() {
+		return userPrincipalService.getUerByUsername(getCurrentUsername());
+	}
+
+	public Authentication getAuthentication() {
+		return getSecurityContext().getAuthentication();
+	}
+
+	public String getCurrentUsername() {
+		String username = null;
+		Object user = getAuthentication().getPrincipal();
+
+		if(user instanceof User && user != null) {
+			username = ((User) user).getUsername();
+		}
+
+		return username;
+	}
+
+	public boolean isAuthenticated() {
+		return getCurrentUsername() != null;
+	}
+}
