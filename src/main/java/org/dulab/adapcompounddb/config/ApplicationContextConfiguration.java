34b03cece2f4c4f94663f5f17b3febd3dae65018
Aleksandr Smirnov <asmirno1@uncc.edu>
2018-09-12 15:16:50 -0400

    Hide Hibernate.SQL debugging info

--- src/main/java/org/dulab/adapcompounddb/config/ApplicationContextConfiguration.java
+++ src/main/java/org/dulab/adapcompounddb/config/ApplicationContextConfiguration.java
@@ -81,7 +81,7 @@ public class ApplicationContextConfiguration {
         jpaPropertyMap.put("javax.persistence.schema-generation.database.action", "none");
 //        jpaPropertyMap.put("hibernate.format_sql", true);
 //        jpaPropertyMap.put("hibernate.use_sql_comments", true);
-        jpaPropertyMap.put("hibernate.show_sql", true);
+//        jpaPropertyMap.put("hibernate.show_sql", true);
 //        jpaPropertyMap.put("hibernate.generate_statistics", true);
         factory.setJpaPropertyMap(jpaPropertyMap);
 
