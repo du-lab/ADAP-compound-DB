329c2ceed10e3010891022d35df4f04e6c65100a
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-05-16 16:51:53 -0400

    Fix a bug with the decorator

--- src/main/java/org/dulab/adapcompounddb/site/filters/CharSetFilter.java
+++ src/main/java/org/dulab/adapcompounddb/site/filters/CharSetFilter.java
@@ -20,7 +20,6 @@ public class CharSetFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         chain.doFilter(request, response);
     }
