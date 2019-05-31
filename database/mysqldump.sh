f429eeca1666169c0836c5d3ac247b8a42b8f0f2
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-05-31 14:18:15 -0400

    Add a script to create a copy of MySQL adapcompounddb databse

---
+++ database/mysqldump.sh
@@ -0,0 +1,1 @@
+mysqldump --user=dulab --host=adapcompounddbrds.cti4tencv7f9.us-east-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 --set-gtid-purged=OFF "adapcompounddb" -p > dump.mysql
