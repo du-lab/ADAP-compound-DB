bbfa87b8801e4d81f421e2342383dd437ae007b8
Eric <ericliaoyf@gmail.com>
2019-06-18 14:50:57 -0400

    no message

--- src/main/webapp/resources/AdapCompoundDb/js/histogram.js
+++ src/main/webapp/resources/AdapCompoundDb/js/histogram.js
@@ -1,12 +1,11 @@
 function addHistogram(idName, dataset) {
 
-    var svgWidth = 500;
-    var svgHeight = 300;
+    var svgWidth = 280;
+    var svgHeight = 280;
     var barPadding = 5;
     var barWidth = (svgWidth / dataset.length);
-    dataset = [10,20,30,40,50,60];
 
-    var svg = d3.select('svg')
+    var svg = d3.select('histogram')
         .attr("width", svgWidth)
         .attr("height", svgHeight);
 
@@ -15,13 +14,14 @@ function addHistogram(idName, dataset) {
         .enter()
         .append("rect")
         .attr("y",function(d){
-        return d;
+        return svgHeight - d;
         })
         .attr("height", function(d){
             return d;
         })
-        .attr("width", barWidth - barpadding)
-        .attr()
-
-
+        .attr("width", barWidth - barPadding)
+        .attr("transform", function(d,i){
+        var translate = [barWidth * i, 0];
+        return "translate("+translate+")";
+    });
 }
