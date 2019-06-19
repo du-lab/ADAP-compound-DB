81691f76df3b95f3ce5e066bcdf82a74d0de908e
Eric <ericliaoyf@gmail.com>
2019-06-19 11:45:37 -0400

    this is the Histogram.js for generating histogram

--- src/main/webapp/resources/AdapCompoundDb/js/histogram.js
+++ src/main/webapp/resources/AdapCompoundDb/js/histogram.js
@@ -1,27 +1,51 @@
-function addHistogram(idName, dataset) {
+function addHistogram(idName, dataSet) {
 
-    var svgWidth = 280;
-    var svgHeight = 280;
-    var barPadding = 5;
-    var barWidth = (svgWidth / dataset.length);
+    var width = 500;
+    var height = 800;
 
-    var svg = d3.select('histogram')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
+    var widthScale = d3.scale.linear()
+        .domain([0,d3.max(dataSet) + 5])
+        .range([0,width]);
 
-    var histogram = svg.selectAll("rect")
-        .data(dataset)
+    var color = d3.scale.linear()
+        .domain([0,82])
+        .range(["yellow","teal"]);
+
+    var axis = d3.svg.axis()
+    /*.ticks(5)*/
+        .scale(widthScale);
+
+    var svg = d3.select("section")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform","translate(5, 10)");
+
+    var bars = svg.selectAll("rect")
+        .data(dataSet)
         .enter()
         .append("rect")
-        .attr("y",function(d){
-        return svgHeight - d;
-        })
-        .attr("height", function(d){
-            return d;
-        })
-        .attr("width", barWidth - barPadding)
-        .attr("transform", function(d,i){
-        var translate = [barWidth * i, 0];
-        return "translate("+translate+")";
-    });
+        .attr("width",function (d) { return widthScale(d); })
+        .attr("height",40)
+        .attr("fill", function (d) { return color(d); })
+        .attr("y", function(d,i){ return i * 60; });
+
+    svg.selectAll("text")
+        .data(dataSet)
+        .enter()
+        .append("text")
+        .attr("dx",function(d){return widthScale(d) / 2;})
+        .attr("dy","1.5em")
+        .attr("y",function(d,i){return i * 60;})
+        .attr("stroke","black")
+        .attr("stroke-width","2")
+        .attr("font-size",15)
+        .text(function(d){return d;});
+
+    svg.append("g")
+        .attr("transform", "translate(0, "+ (dataSet.length) * 57 +")")
+        .call(axis);
+
+
 }
