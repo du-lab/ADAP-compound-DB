3367ed1add5a22061cac3bed620d73d20a424a38
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-07-24 14:39:31 -0400

    Add a script to truncate (almost) all tables in the database

--- database/mysqldump.sh
+++ database/mysqldump.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 mysqldump --user=dulab --host=adapcompounddbrds.cti4tencv7f9.us-east-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 --set-gtid-purged=OFF "adapcompounddb" -p > dump.mysql
---
+++ database/truncate__all_tables.sql
@@ -0,0 +1,17 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+TRUNCATE `adapcompounddb`.`DiversityIndex`;
+TRUNCATE `adapcompounddb`.`File`;
+TRUNCATE `adapcompounddb`.`Peak`;
+TRUNCATE `adapcompounddb`.`Spectrum`;
+TRUNCATE `adapcompounddb`.`SpectrumCluster`;
+TRUNCATE `adapcompounddb`.`SpectrumMatch`;
+TRUNCATE `adapcompounddb`.`SpectrumProperty`;
+TRUNCATE `adapcompounddb`.`Submission`;
+TRUNCATE `adapcompounddb`.`Submission2SubmissionCategory`;
+TRUNCATE `adapcompounddb`.`SubmissionCategory`;
+TRUNCATE `adapcompounddb`.`SubmissionTag`;
+TRUNCATE `adapcompounddb`.`TagDistribution`;
+TRUNCATE `adapcompounddb`.`UserParameter`;
+
+SET FOREIGN_KEY_CHECKS=1;
