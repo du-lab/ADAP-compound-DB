00753c5ffc8005032c7a42f94774957803c30b15
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-10-04 15:33:35 -0400

    Clean up the source code

--- src/main/java/org/dulab/adapcompounddb/site/services/SpectrumClusterer.java
+++ src/main/java/org/dulab/adapcompounddb/site/services/SpectrumClusterer.java
@@ -1,18 +1,11 @@
 package org.dulab.adapcompounddb.site.services;
 
-import org.dulab.adapcompounddb.models.entities.Spectrum;
-import org.dulab.adapcompounddb.models.entities.SpectrumCluster;
-import org.dulab.adapcompounddb.models.entities.TagDistribution;
 import org.springframework.validation.annotation.Validated;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 @Validated
 public interface SpectrumClusterer {
 
-    void cluster(/*ChromatographyType type, int minNumSpectra, float scoreTolerance, float mzTolerance*/);
+    void cluster();
 
     void removeAll();
 
