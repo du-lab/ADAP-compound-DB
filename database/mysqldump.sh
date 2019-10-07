d911adc703ef6c6f0aa76814bf024abbcad8328e
Aleksandr Smirnov <asmirno1@uncc.edu>
2019-10-07 16:08:23 -0400

    Add a comment to mysqldump script

--- database/mysqldump.sh
+++ database/mysqldump.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 mysqldump --user=dulab --host=adapcompounddbrds.cti4tencv7f9.us-east-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 --set-gtid-purged=OFF -p --add-drop-database --databases "adapcompounddb" > dump.mysql
+
+
+# To upload the backup, run
+# mysql -u root -p < dump.mysql
