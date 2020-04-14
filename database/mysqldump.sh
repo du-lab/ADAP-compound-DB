5ac4e060493f75693bac983cc569431c688383e4
Aleksandr Smirnov <smirnov09al@gmail.com>
2020-04-14 09:53:03 -0400

    Fix the bug when mysqldump v8 didn't work

--- database/mysqldump.sh
+++ database/mysqldump.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mysqldump --user=dulab --host=adapcompounddbrds.cti4tencv7f9.us-east-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 --set-gtid-purged=OFF -p --add-drop-database --databases "adapcompounddb" > dump.mysql
+mysqldump --user=dulab --host=adapcompounddbrds.cti4tencv7f9.us-east-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 --column-statistics=0 --set-gtid-purged=OFF -p --add-drop-database --databases "adapcompounddb" > dump.mysql
 
 
 # To upload the backup, run
