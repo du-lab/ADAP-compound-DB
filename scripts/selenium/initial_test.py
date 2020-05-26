d26d1da0e2791552daee04897db781a45bc3cf9c
Aleksandr Smirnov <asmirno1@uncc.edu>
2020-05-26 14:10:01 -0400

    Update initial_test.py

--- scripts/selenium/initial_test.py
+++ scripts/selenium/initial_test.py
@@ -6,13 +6,12 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome('scripts/selenium/drivers/chromedriver')
 
 # Open a web page
-driver.get("https://www.python.org")
+driver.get("http://localhost:8080/adap_compound_db_war/")
 
 # Print out a title of the web page
 print(driver.title)
 
 # Close the browser
-driver.close()
 driver.quit()
 
 print('Test is complete')
