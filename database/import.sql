ff11fc8b62b33901773ef2352b9029dbe4853295
Aleksandr Smirnov <smirnov09al@gmail.com>
2020-06-02 09:44:01 -0400

    Update the script for importing similarity matches

--- database/import.sql
+++ database/import.sql
@@ -1,1 +1,5 @@
-LOAD DATA LOCAL INFILE '/Users/aleksandrsmirnov/Projects/du-lab/kdtree/data/matches_2020-04-14.filtered.csv' INTO TABLE SpectrumMatch FIELDS TERMINATED BY ',' IGNORE 1 LINES (QuerySpectrumId, MatchSpectrumId, Score);
+LOAD DATA LOCAL INFILE '/Users/aleksandrsmirnov/Projects/du-lab/kdtree/data/matches_2020-06-01.filtered.csv' INTO TABLE SpectrumMatch FIELDS TERMINATED BY ',' IGNORE 1 LINES (QuerySpectrumId, MatchSpectrumId, Score);
+
+-- You may need to execute the folloing lines first:
+-- USE adapcompounddb;
+-- SET GLOBAL local_infile=1;
