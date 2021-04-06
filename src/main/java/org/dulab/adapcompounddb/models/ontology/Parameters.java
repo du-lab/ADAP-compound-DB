0a831e22df910c1d4a0f27abe47a97bf33a2dc87
Aleksandr Smirnov <smirnov09al@gmail.com>
2021-04-06 15:39:20 -0400

    Modify the m/z tolerance for ontology levels

--- src/main/java/org/dulab/adapcompounddb/models/ontology/Parameters.java
+++ src/main/java/org/dulab/adapcompounddb/models/ontology/Parameters.java
@@ -1,7 +1,7 @@
 package org.dulab.adapcompounddb.models.ontology;
 
 public class Parameters {
-    static final double MZ_TOLERANCE = 0.001;
+    static final double MZ_TOLERANCE = 0.01;
     static final double SCORE_THRESHOLD = 0.3;
     static final double PRECURSOR_TOLERANCE = 0.01;
 //    static final double MASS_TOLERANCE = 0.01;
