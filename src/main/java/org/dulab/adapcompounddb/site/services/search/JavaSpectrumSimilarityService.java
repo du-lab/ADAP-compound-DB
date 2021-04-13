ab1e2e3307d061483ef7c317e283504d86eca936
Eric Liao <ericliaoyf@gmail.com>
2021-04-13 12:14:32 -0400

    fix the error for iterator

--- src/main/java/org/dulab/adapcompounddb/site/services/search/JavaSpectrumSimilarityService.java
+++ src/main/java/org/dulab/adapcompounddb/site/services/search/JavaSpectrumSimilarityService.java
@@ -17,16 +17,13 @@ public class JavaSpectrumSimilarityService {
 
         // iterate each spectrum in the adap-kdb library
         //TODO It should work without n and sizeOfLibrarySpectra. I don't think you need these.
-        int n = 0;
-        int sizeOfLibrarySpectra = ((ArrayList) librarySpectra).size();
-        while (librarySpectrumIterator.hasNext() && n <= sizeOfLibrarySpectra) {
-            n++;
+        while (librarySpectrumIterator.hasNext() ) {
             //TODO Replace `librarySpectra.iterator()` with `librarySpectrumIterator`
             // Every time when you call `librarySpectra.iterator()`, a new iterator is created pointing to the first library spectrum.
             // So `librarySpectra.iterator().next()` gives you the same spectrum every time.
-            Spectrum librarySpectrum = librarySpectra.iterator().next();
+            Spectrum librarySpectrum = librarySpectrumIterator.next();
             //TODO Replace `List<Double> productList` with `double sum = 0.0`
-            List<Double> productList = new ArrayList<>();
+            double sum = 0.0;
             // iterate each peak of query spectrum and current library spectrum, and calculate the product value
             for (Peak p1 : querySpectrum.getPeaks()) {
                 double queryIntensity = p1.getIntensity();
@@ -35,17 +32,18 @@ public class JavaSpectrumSimilarityService {
                     double libraryIntensity = p2.getIntensity();
                     double libraryMz = p2.getMz();
                     //TODO You can move the product calculation inside the if-statement. It's a waste of time to calculate this product for unmatched peaks
-                    double product = Math.sqrt(queryIntensity * libraryIntensity);
+
                     // if ABS(Mz - 𝑚𝑧𝑛) < MzTolerance, then add it to the product list for calculate similarity score later
                     if (Math.abs(libraryMz - queryMz) < parameters.getMzTolerance()) {
+                        double product = Math.sqrt(queryIntensity * libraryIntensity);
                         //TODO Replace this line with `sum += product`
-                        productList.add(product);
+                        sum += product;
                     }
                 }
             }
             // calculate the similarity score
             //TODO Replace `productList.stream().mapToDouble(Double::doubleValue).sum()` with `sum`
-            double similarityScore = Math.pow(productList.stream().mapToDouble(Double::doubleValue).sum(), 2);
+            double similarityScore = Math.pow(sum, 2);
 
             // if the similarity score > ScoreThreshold, then return the MatchSpectrum
             if (similarityScore > parameters.getScoreThreshold()) {
