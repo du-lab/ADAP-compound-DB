ebe4e21a977b79073fa84761a27e627c18e0909e
Aleksandr Smirnov <asmirno1@uncc.edu>
2021-08-12 15:23:56 -0400

    Increase the maximum file upload size

--- src/main/java/org/dulab/adapcompounddb/config/ServletContextConfiguration.java
+++ src/main/java/org/dulab/adapcompounddb/config/ServletContextConfiguration.java
@@ -34,7 +34,7 @@ public class ServletContextConfiguration implements WebMvcConfigurer {
     @Bean
     public MultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-        resolver.setMaxUploadSizePerFile(128 * 1024 * 1024);  // 128MB
+        resolver.setMaxUploadSizePerFile(1024 * 1024 * 1024);  // 1GB
         return resolver;
     }
 }
