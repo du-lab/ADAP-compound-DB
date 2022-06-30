ac3e9c1c79d0b47f302784e574fd2fcf4e12be95
Varun <vsuresh1@uncc.edu>
2022-06-30 16:46:18 -0400

    Delete all files in folder before exporting

--- scripts/export_to_txt.py
+++ scripts/export_to_txt.py
@@ -1,8 +1,12 @@
 import os
 import argparse
 from datetime import datetime
+import shutil
 
 def export(user, password, database, folder):
+	files = [f for f in os.listdir(folder)]
+	for f in files:
+		os.system(f'sudo rm {os.path.join(folder,f)}')
 	os.system(f'sudo mysqldump -u {user} -p{password} --tab {folder} {database}')
 	file_name = 'backup_' + datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
 	os.system(f'sudo zip -r {file_name}.zip {folder}')
