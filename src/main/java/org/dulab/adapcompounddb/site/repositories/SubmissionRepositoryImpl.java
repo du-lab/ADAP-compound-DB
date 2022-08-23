514027c503dcec0ccac97d761c0fef5637c69047
Aleksandr Smirnov <asmirno1@uncc.edu>
2022-08-23 10:00:50 -0400

    Add isotopes to to the repository method for fetching spectra

--- src/main/java/org/dulab/adapcompounddb/site/repositories/SubmissionRepositoryImpl.java
+++ src/main/java/org/dulab/adapcompounddb/site/repositories/SubmissionRepositoryImpl.java
@@ -1,15 +1,16 @@
 package org.dulab.adapcompounddb.site.repositories;
 
-import org.dulab.adapcompounddb.models.entities.File;
-import org.dulab.adapcompounddb.models.entities.Peak;
-import org.dulab.adapcompounddb.models.entities.Spectrum;
-import org.dulab.adapcompounddb.models.entities.Submission;
+import org.dulab.adapcompounddb.models.entities.*;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class SubmissionRepositoryImpl implements SubmissionRepositoryCustom {
@@ -29,27 +30,36 @@ public class SubmissionRepositoryImpl implements SubmissionRepositoryCustom {
                 .getResultList();
 
         List<Spectrum> spectra = entityManager
-                .createQuery("select s from Spectrum s where s.file.submission.id = :submissionId", Spectrum.class)
-                .setParameter("submissionId", submissionId)
+                .createQuery("select s from Spectrum s where s.file.id in (:fileIds)", Spectrum.class)
+                .setParameter("fileIds", files.stream().map(File::getId).collect(Collectors.toList()))
                 .getResultList();
 
         List<Peak> peaks = entityManager
-                .createQuery("select p from Peak p where p.spectrum.file.submission.id = :submissionId", Peak.class)
-                .setParameter("submissionId", submissionId)
+                .createQuery("select p from Peak p where p.spectrum.id in (:spectrumIds)", Peak.class)
+                .setParameter("spectrumIds", spectra.stream().map(Spectrum::getId).collect(Collectors.toList()))
                 .getResultList();
 
-        Map<Spectrum, List<Peak>> spectrumToPeakListMap = peaks.stream()
-                .collect(Collectors.groupingBy(Peak::getSpectrum));
-        spectrumToPeakListMap.forEach(Spectrum::setPeaks);
+        List<Isotope> isotopes = entityManager
+                .createQuery("select i from Isotope i where i.spectrum.id in (:spectrumIds)", Isotope.class)
+                .setParameter("spectrumIds", spectra.stream().map(Spectrum::getId).collect(Collectors.toList()))
+                .getResultList();
 
-        Map<File, List<Spectrum>> fileToSpectrumListMap = spectrumToPeakListMap.keySet().stream()
-                .collect(Collectors.groupingBy(Spectrum::getFile));
-        fileToSpectrumListMap.forEach(File::setSpectra);
-
-        Map<Submission, List<File>> submissionToFileListMap = fileToSpectrumListMap.keySet().stream()
-                .collect(Collectors.groupingBy(File::getSubmission));
-        submissionToFileListMap.forEach(Submission::setFiles);
+        assignChildrenToParents(peaks, Peak::getSpectrum, spectra, Spectrum::setPeaks);
+        assignChildrenToParents(isotopes, Isotope::getSpectrum, spectra, Spectrum::setIsotopes);
+        assignChildrenToParents(spectra, Spectrum::getFile, files, File::setSpectra);
+        assignChildrenToParents(files, File::getSubmission, Collections.singletonList(submission), Submission::setFiles);
 
         return submission;
+    }
+
+    private <C, P> void assignChildrenToParents(List<C> children, Function<C, P> parentGetter,
+                                                List<P> parents, BiConsumer<P, List<C>> childrenSetter) {
+
+        Map<P, List<C>> parentToChildrenMap = children.stream()
+                .collect(Collectors.groupingBy(parentGetter));
+
+        for (P parent : parents) {
+            childrenSetter.accept(parent, parentToChildrenMap.get(parent));
+        }
     }
 }
