07bddf41e0cbe846d9c81edb6af9b784d2a3a95e
Varun Suresh <vasur2201@gmail.com>
2022-08-29 14:31:22 -0400

    Added identifier argument

--- scripts/selenium/manual_search_test.py
+++ scripts/selenium/manual_search_test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import argparse
 import time
 
-def manual_search_test(homepage_url):
+def manual_search_test(homepage_url, identifier):
 	driver = webdriver.Chrome('scripts/selenium/drivers/chromedriver')
 
 	try:
@@ -10,7 +10,7 @@ def manual_search_test(homepage_url):
 		search_page_button = driver.find_element_by_id('manualSearchPage')
 		search_page_button.click()
 		identifier_input = driver.find_element_by_id('identifierInput')
-		identifier_input.send_keys('eicosatrienoic acid')
+		identifier_input.send_keys(identifier)
 		#spectrum_input = driver.get_element_by_id('spectrumInput')
 		#spectrum_input.send_keys('79 100; 67 90.3325; 80 75.8722; 81 59.0838; 93 54.0933')
 		search_button = driver.find_element_by_id('searchButton')
@@ -19,7 +19,7 @@ def manual_search_test(homepage_url):
 		assert (driver.find_element_by_id('plot'))
 
 	except Exception as e:
-		#driver.quit()
+		driver.quit()
 		raise e
 
 
@@ -28,11 +28,12 @@ def main():
 
 	parser = argparse.ArgumentParser('Download all data into a folder')
 	parser.add_argument('--homepage_url', help='url for adap-kdb homepage', required=True)
+	parser.add_argument('--identifier', help='identifier string', required=True)
 	args = parser.parse_args()
-
 	homepage_url = args.homepage_url
+	identifier = args.identifier
 
-	manual_search_test(homepage_url)
+	manual_search_test(homepage_url, identifier)
 
 if __name__ == '__main__':
 	main()
