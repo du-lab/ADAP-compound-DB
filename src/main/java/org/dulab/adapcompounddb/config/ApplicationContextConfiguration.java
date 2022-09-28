84f3492ad86ab0c34c8d3d7f1d6ddd9a7f4fc63e
tnguyen271 <tnguy271@uncc.edu>
2022-09-28 15:10:09 -0400

    init commit

--- src/main/java/org/dulab/adapcompounddb/config/ApplicationContextConfiguration.java
+++ src/main/java/org/dulab/adapcompounddb/config/ApplicationContextConfiguration.java
@@ -21,11 +21,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -134,4 +137,22 @@ public class ApplicationContextConfiguration {
         executor.setCorePoolSize(8);
         return executor;
     }
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
 }
---
+++ src/main/java/org/dulab/adapcompounddb/rest/controllers/EmailController.java
@@ -0,0 +1,32 @@
+package org.dulab.adapcompounddb.rest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+
+    @PostMapping(value = "/sendEmail")
+    public ResponseEntity<String> sendEmail(final @RequestBody CommonsMultipartFile file) {
+        //JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("tnguy271@uncc.edu");
+
+        msg.setSubject("Testing email");
+        msg.setText("Hello World \n ");
+
+        javaMailSender.send(msg);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+}
