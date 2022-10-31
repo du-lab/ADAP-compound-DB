5ba6e06ca7ede68e2b1576d62ebb6113997246a0
tnguy271 <tnguy271@uncc.edu>
2022-10-31 11:02:15 -0400

    revert changes to use python script to draw molecule

--- src/main/java/org/dulab/adapcompounddb/site/controllers/SpectrumRestController1.java
+++ src/main/java/org/dulab/adapcompounddb/site/controllers/SpectrumRestController1.java
@@ -115,7 +115,7 @@ public class SpectrumRestController1 {
 
             JSONObject smilesImage = new JSONObject();
 
-                smilesImage.put("image", ConversionsUtils.toImage(spectrum.getCanonicalSmiles(), spectrum.getInChi()));
+                smilesImage.put("image", ConversionsUtils.toImagePython(spectrum.getCanonicalSmiles(), spectrum.getInChi()));
 
             return smilesImage.toString();
         } catch (EmptySearchResultException e) {
