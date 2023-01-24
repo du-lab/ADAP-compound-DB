20a29a0b8098ac284f9fd00f442aae64a06d25a0
Toan Nguyen <tnguy271@uncc.edu>
2023-01-24 12:43:24 -0500

    modify initial_test.py

--- scripts/selenium/initial_test.py
+++ scripts/selenium/initial_test.py
@@ -8,6 +8,8 @@ driver = webdriver.Chrome('scripts/selenium/drivers/chromedriver.exe')
 # Open a web page
 driver.get("http://localhost:8080/")
 
+assert driver.title == 'ADAP-KDB Compound Knowledgebase'
+
 # Print out a title of the web page
 print(driver.title)
 
