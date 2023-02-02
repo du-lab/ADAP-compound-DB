f07c3ab71240648c5359efcf05ae8b966475f6a7
Shanmukh Gorle <123002254+Shanmukh801311172@users.noreply.github.com>
2023-02-02 17:06:46 -0500

    changed search parameters sql file

--- database/search_parameters.sql
+++ database/search_parameters.sql
@@ -1,14 +1,8 @@
 CREATE TABLE `adapcompounddb`.`searchparameters` (
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(45) NOT NULL,
     `value` TEXT NOT NULL,
     `UserPrimaryId` BIGINT UNSIGNED NULL,
-    INDEX `Id_idx` (`UserPrimaryId` ASC) VISIBLE,
-    CONSTRAINT `Id`
-        FOREIGN KEY (`UserPrimaryId`)
-            REFERENCES `adapcompounddb`.`userprincipal` (`Id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION);
-ALTER TABLE `adapcompounddb`.`searchparameters`
-    ADD COLUMN `id` VARCHAR(45) NOT NULL AFTER `UserPrimaryId`,
-ADD PRIMARY KEY (`id`);
-;
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`UserPrimaryId`)
+        REFERENCES `adapcompounddb`.`userprincipal` (`Id`));
