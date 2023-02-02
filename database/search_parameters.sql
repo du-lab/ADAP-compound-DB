b2e18ccc836bbe25b51eda4fd7852511b52160e6
Shanmukh Gorle <123002254+Shanmukh801311172@users.noreply.github.com>
2023-02-02 12:22:47 -0500

    fixed sql file db name

--- database/search_parameters.sql
+++ database/search_parameters.sql
@@ -1,14 +1,14 @@
-CREATE TABLE `adapcompounddb_updated`.`searchparameters` (
+CREATE TABLE `adapcompounddb`.`searchparameters` (
     `name` VARCHAR(45) NOT NULL,
     `value` TEXT NOT NULL,
     `UserPrimaryId` BIGINT UNSIGNED NULL,
     INDEX `Id_idx` (`UserPrimaryId` ASC) VISIBLE,
     CONSTRAINT `Id`
         FOREIGN KEY (`UserPrimaryId`)
-            REFERENCES `adapcompounddb_updated`.`userprincipal` (`Id`)
+            REFERENCES `adapcompounddb`.`userprincipal` (`Id`)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION);
-ALTER TABLE `adapcompounddb_updated`.`searchparameters`
+ALTER TABLE `adapcompounddb`.`searchparameters`
     ADD COLUMN `id` VARCHAR(45) NOT NULL AFTER `UserPrimaryId`,
 ADD PRIMARY KEY (`id`);
 ;
