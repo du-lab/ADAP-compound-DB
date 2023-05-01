ba2bd1d68dfa98cfe58329267bd7e3c1ba3e9486
Toan Nguyen <tnguy271@uncc.edu>
2023-05-01 16:18:23 -0400

    added jsp to handle 404 error

--- src/main/java/org/dulab/adapcompounddb/site/controllers/ErrorHandlingController.java
+++ src/main/java/org/dulab/adapcompounddb/site/controllers/ErrorHandlingController.java
@@ -1,5 +1,6 @@
 package org.dulab.adapcompounddb.site.controllers;
 
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -8,23 +9,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Controller
 public class ErrorHandlingController implements ErrorController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ErrorHandlingController.class);
 
-
+    private static String pageNotFoundMsg = "The page you requested was not found.";
     @RequestMapping(value = "/error")
     public String error(@RequestParam(name = "errorMsg", required = false) String message,
-                        Model model) {
+                        Model model, HttpServletRequest request, HttpServletResponse response) {
 
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value() ) {
+                response.setStatus(HttpStatus.OK.value());
+                model.addAttribute("errorMsg", pageNotFoundMsg );
+                return "redirect:404";
+            }
+        }
         if (message != null) {
             model.addAttribute("errorMsg", message);
         }
-
         return "error";
+    }
+    @RequestMapping(value="/404")
+    public String pageNotFoundHandler(){
+        return "404";
     }
 
     @RequestMapping(value = "/js-log", method = RequestMethod.POST)
---
+++ src/main/webapp/WEB-INF/jsp/view/404.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tnguy271
+  Date: 5/1/23
+  Time: 4:12 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<div class="container">
+    <div class="row row-content">
+        <div class="col">
+            <div class="card">
+                <div class="card-header card-header-single">
+                    Error
+                </div>
+                <div class="card-body">
+                        <p class="errors">Message: The page you are looking for is not found.</p>
+                    <p>
+                        Please contact the support team if this error occurs again:
+                        <a href="mailto:adap.helpdesk@gmail.com">adap.helpdesk@gmail.com</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
