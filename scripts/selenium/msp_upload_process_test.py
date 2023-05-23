ac02a553dd9da41d0b1b03811bb49e28806200ac
Shanmukh Gorle <46778952+gorleshanmukh@users.noreply.github.com>
2023-05-23 15:47:53 -0400

    Update msp_upload_process_test.py

--- scripts/selenium/msp_upload_process_test.py
+++ scripts/selenium/msp_upload_process_test.py
@@ -6,7 +6,7 @@ import argparse
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from urllib.parse import urljoin
-
+import time
 
 def upload_process_test(homepage_url, msp_path):
     driver = webdriver.Chrome('scripts/selenium/drivers/chromedriver')
@@ -22,9 +22,9 @@ def upload_process_test(homepage_url, msp_path):
         submit_button = driver.find_element_by_name("submit")
         option_bar.select_by_visible_text('GC')
         choose_key.send_keys(msp_path)
-        driver.find_element_by_id("uploadBtn").click()
         submit_button.click()
-
+        time.sleep(2)
+        driver.find_element_by_id("uploadBtn").click()
         assert (driver.current_url.__str__().startswith(urljoin(homepage_url, 'file/')))
 
     except Exception as e:
