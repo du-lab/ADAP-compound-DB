d045fa79e0c4337cf4b8e19be8f851460f97062e
Aleksandr Smirnov <asmirno1@uncc.edu>
2023-09-14 12:54:03 -0400

    Reverse temporary changes

--- scripts/selenium/login_register_test.py
+++ scripts/selenium/login_register_test.py
@@ -81,6 +81,7 @@ def login_register_test(homepage_url, username_value, password_value):
 def delete_account(homepage_url, username, password):
     try:
         driver.get(homepage_url)
+        time.sleep(5)
         # go to login page
         driver.find_element("id", 'loginPage').click()
         time.sleep(5)
@@ -97,12 +98,12 @@ def delete_account(homepage_url, username, password):
         driver.find_element(By.XPATH, "//button[text()='Confirm']").click()
         time.sleep(1)
     except Exception as e:
-#         driver.quit()
+        driver.quit()
         raise e
 
 def logout():
     time.sleep(1)
-    driver.find_element('id','logoutPage').click();
+    driver.find_element('id','logoutPage').click()
     time.sleep(1)
 
 def main():
