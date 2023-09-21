808ed4c12eb4c28799b221b0cb99ac1eab155631
Shanmukh Gorle <46778952+gorleshanmukh@users.noreply.github.com>
2023-09-21 18:40:36 -0400

    Update individual_search_test_after_login.py

--- scripts/selenium/individual_search_test_after_login.py
+++ scripts/selenium/individual_search_test_after_login.py
@@ -6,6 +6,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 
 def individual_search_test(homepage_url, msp_path, user_name, user_password):
@@ -15,64 +16,64 @@ def individual_search_test(homepage_url, msp_path, user_name, user_password):
         driver.get(homepage_url)
 
         # go to login page
-        upload_page_button = driver.find_element_by_id('loginPage')
+        upload_page_button = driver.find_element('id','loginPage')
         upload_page_button.click()
 
         # log in with new created user information
-        username_login = driver.find_element_by_id('username')
+        username_login = driver.find_element('id','username')
         username_login.send_keys(user_name)
 
-        password_login = driver.find_element_by_id('password')
+        password_login = driver.find_element('id','password')
         password_login.send_keys(user_password)
 
-        driver.find_element_by_name("submit").click()
+        driver.find_element('name', "submit").click()
 
         # upload msp file
-        upload_page_button = driver.find_element_by_id('uploadPage')
+        upload_page_button = driver.find_element('id','uploadPage')
         upload_page_button.click()
 
-        option_bar = Select(driver.find_element_by_id('chromatographyType'))
-        choose_key = driver.find_element_by_name('files')
-        submit_button = driver.find_element_by_name("submit")
+        option_bar = Select(driver.find_element('id','chromatographyType'))
+        choose_key = driver.find_element('name', 'files')
+        submit_button = driver.find_element('name', "submit")
         option_bar.select_by_visible_text('GC')
         choose_key.send_keys(msp_path)
         submit_button.click()
 
         # add 5 seconds delay for msp file to upload before next step
         time.sleep(5)
-        driver.find_element_by_id("uploadBtn").click()
+        driver.find_element('id',"uploadBtn").click()
         time.sleep(2)
         
 
         # click Mass Spectra tab
-        mass_spectra_tab = driver.find_element_by_id("mass_spectra_link")
+        mass_spectra_tab = driver.find_element('id',"mass_spectra_link")
         mass_spectra_tab.click()
 
         # choose the first spectrum and go to the spectrum page
-        spectra_table = driver.find_element_by_id("spectrum_table")
-        spectra_list = spectra_table.find_elements_by_css_selector("table>tbody>tr>td")
+        spectra_table = driver.find_element('id',"spectrum_table")
+        spectra_list = spectra_table.find_elements(By.CSS_SELECTOR,  "table>tbody>tr>td")
         # choose the spectrum link
-        spectrum_link = spectra_list[1].find_element_by_css_selector('a')
+        spectrum_link = spectra_list[1].find_element(By.CSS_SELECTOR,  'a')
         spectrum_link.click()
 
         # click search button on spectrum page
-        search_button = driver.find_element_by_id('searchButton')
+        search_button = driver.find_element('id','searchButton')
         search_button.click()
 
         # click search button on single search page
 #         search_button = driver.find_element_by_class_name('button')
-        search_button = driver.find_element_by_id('searchButton')
+        search_button = driver.find_element('id','searchButton')
         search_button.click()
 
         # add 10 seconds delay for spectrum search complete before next step
         time.sleep(10)
 
         # check if spectrum figure is plotted
-        assert (driver.find_element_by_id('plot'))
+        assert (driver.find_element('id','plot'))
 
         # check if the matching table contains values
-        matching_table = driver.find_element_by_id('table')
-        data_list = matching_table.find_elements_by_css_selector('table>tbody>tr')
+        matching_table = driver.find_element('id','table')
+        data_list = matching_table.find_elements(By.CSS_SELECTOR,  'table>tbody>tr')
         assert data_list
 
     except Exception as e:
