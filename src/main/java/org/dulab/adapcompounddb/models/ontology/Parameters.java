88187f212ec317251c71d7734a82a1b352b92bc9
Toan Nguyen <>
2025-05-12 15:39:54 -0400

    hide default parameters and add ontology level parameters to search result when search with ontology levels

--- src/main/java/org/dulab/adapcompounddb/models/ontology/Parameters.java
+++ src/main/java/org/dulab/adapcompounddb/models/ontology/Parameters.java
@@ -7,4 +7,14 @@ public class Parameters {
     public static final int MASS_TOLERANCE_PPM = 5;
     public static final double RET_TIME_TOLERANCE = 0.5;
     public static final double ISOTOPIC_SIMILARITY_THRESHOLD = 0.9;
+
+    public static String getSearchParametersAsString() {
+        final String COMMA = ", ";
+        return "M/Z Tolerance (PPM) = " + MZ_TOLERANCE_PPM + COMMA +
+                "Precursor Tolerance (PPM) = " + PRECURSOR_TOLERANCE_PPM + COMMA +
+                "Mass Tolerance (PPM) = " + MASS_TOLERANCE_PPM + COMMA +
+                "Retention Time Tolerance = " + RET_TIME_TOLERANCE + COMMA +
+                "Isotopic Similarity Threshold = " + ISOTOPIC_SIMILARITY_THRESHOLD + COMMA +
+                "Score Threshold = " + SCORE_THRESHOLD;
+    }
 }
